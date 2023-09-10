258383c724c47f0f3400a60bfd5abf6f56e4afc5
wh <884923319@qq.com>
2023-09-10 11:56:31 +0800

    fix(web): 优化EditInput组件

--- web/src/components/EditInput.vue
+++ web/src/components/EditInput.vue
@@ -2,8 +2,10 @@
   <div
     class="editable-box"
     :style="{}"
-    @input="handleChange"
-    @keydown.enter="handle"
+    @input="handleInput"
+    @keydown="handleKeyDown"
+    @focus="handleFocus"
+    @blur="handleBlur"
     contenteditable="true"
   >
     <p
@@ -12,18 +14,17 @@
           ? props.textBoxStyle
           : [props.textBoxStyle, { color: 'gray' }]
       "
-    >
-      {{ props.modelValue || props.placeholder }}
-    </p>
+      v-html="inputValue"
+    ></p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { type PropType, type StyleValue } from "vue";
+import { ref, type PropType, type StyleValue } from "vue";
 const props = defineProps({
   modelValue: {
     type: String,
-    required: true,
+    required: true
   },
   placeholder: {
     type: String,
@@ -34,30 +35,47 @@ const props = defineProps({
     default: () => ({ padding: "8px" })
   }
 });
+const inputValue = ref(props.modelValue || props.placeholder);
 
-function handle(e: Event) {
-  e.preventDefault();
+function handleKeyDown(e: KeyboardEvent) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+  }
+
   // emit("update:modelValue", props.modelValue + "</br>");
+}
+
+function handleFocus() {
+  if (props.modelValue === "") {
+    inputValue.value = "<br/>";
+  }
+}
+
+function handleBlur() {
+  if (props.modelValue === "") {
+    inputValue.value = props.placeholder;
+  }
 }
 
 const emit = defineEmits(["update:modelValue"]);
 
-function handleChange(e: Event) {
+function handleInput(e: Event) {
   // console.log(e);
 
   if (e.target) {
-    if (props.modelValue === "") {
-      emit("update:modelValue", (e as InputEvent).data);      
-      
-    } else {
-      emit("update:modelValue", (e.target as HTMLInputElement).textContent);
-    }
+    // if (props.modelValue === "") {
+    // console.log((e as InputEvent).data);
+    // inputValue.value = (e as InputEvent).data as string;
+    //   emit("update:modelValue", (e as InputEvent).data);
+    // } else {
+    // console.log((e.target as HTMLInputElement).textContent);
+    emit("update:modelValue", (e.target as HTMLInputElement).textContent);
+    // }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
 .editable-box:hover {
   border: 1px dashed rgb(64, 160, 220);
 }
