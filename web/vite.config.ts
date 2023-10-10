ea5f466a00abaf98f231258ba3c291f0d51db99f
wh <884923319@qq.com>
2023-10-10 23:10:49 +0800

    feat: 构建打包

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,22 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import ElementPlus from "unplugin-element-plus/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ElementPlus({})],
+  plugins: [vue(), ElementPlus({}), visualizer({ open: true })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: "[ext]/[name]-[hash].[ext]"
+      }
     }
   },
   server: {
