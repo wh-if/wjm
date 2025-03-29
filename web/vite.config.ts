373f8eea0ee9f0bf275af9b0b4b9f34db660e88a
wh-if <884923319@qq.com>
2025-03-29 17:17:38 +0800

    feat: 添加 Husky 和 lint-staged 支持，优化代码格式

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,38 +1,33 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import ElementPlus from "unplugin-element-plus/vite";
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import ElementPlus from 'unplugin-element-plus/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), ElementPlus({}), visualizer({ open: true })],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   build: {
     rollupOptions: {
       output: {
-        chunkFileNames: "js/[name]-[hash].js",
-        entryFileNames: "js/[name]-[hash].js",
-        assetFileNames: "[ext]/[name]-[hash].[ext]"
-      }
-    }
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+      },
+    },
   },
   server: {
     port: 8081,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        rewrite: (path) => path.replace(/^\/api/, "")
-        // secure: false,
-        // headers: {
-        //   Referer: 'https://shop.vivo.com.cn',
-        //   Host: 'shop.vivo.com.cn'
-        // }
-      }
-    }
-  }
+      '/api': {
+        target: 'http://localhost:8080',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
