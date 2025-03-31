41b1734722b00ca33194c7fde50e2551e65ddbaf
wh <884923319@qq.com>
2025-03-31 15:18:56 +0800

    feat: 完善 ESLint and Prettier 配置、更新依赖版本

--- server/src/controller/FileController.ts
+++ server/src/controller/FileController.ts
@@ -1,25 +1,29 @@
-import koaBody from "koa-body";
-import { Controller, HttpMethodEnum } from "../model";
-import path from "path";
-import { AjaxResult } from "../util/AjaxResult";
-import send from "koa-send";
-import fs from "fs/promises";
+import koaBody from 'koa-body';
+import { Controller, HttpMethodEnum } from '../model';
+import path from 'path';
+import { AjaxResult } from '../util/AjaxResult';
+import send from 'koa-send';
+import fs from 'fs/promises';
 const controller: Controller[] = [
   // 需要访问权限
   {
-    path: "/file/upload",
+    path: '/file/upload',
     method: HttpMethodEnum.POST,
     handler: [
       koaBody({
         multipart: true,
         formidable: {
-          uploadDir: path.join(__dirname, "../upload"),
+          uploadDir: path.join(__dirname, '../upload'),
           keepExtensions: true,
           maxFieldsSize: 2 * 1024 * 1024,
         },
       }),
       (ctx) => {
-        const file = ctx.request.files.file as any;
+        const file = ctx.request.files.file;
+        if (Array.isArray(file)) {
+          // TODO
+          return;
+        }
         ctx.body = AjaxResult.success({
           mimetype: file.mimetype,
           filename: file.newFilename,
@@ -30,29 +34,29 @@ const controller: Controller[] = [
   },
   // 需要访问权限
   {
-    path: "/file/target/:id",
+    path: '/file/target/:id',
     method: HttpMethodEnum.GET,
     handler: [
       async (ctx, next) => {
-        const srcPath = "/" + ctx.params.id;
+        const srcPath = '/' + ctx.params.id;
         const opts: send.SendOptions = {
-          root: path.join(__dirname, "../upload"),
+          root: path.join(__dirname, '../upload'),
         };
         await send(ctx, srcPath, opts);
-        ctx.set("Content-Type", ctx.request.header["accept"]);
+        ctx.set('Content-Type', ctx.request.header['accept']);
         next();
       },
     ],
   },
   // 公开的，不需要权限访问,上传
   {
-    path: "/public",
+    path: '/public',
     method: HttpMethodEnum.POST,
     handler: [
       koaBody({
         multipart: true,
         formidable: {
-          uploadDir: path.join(__dirname, "../public"),
+          uploadDir: path.join(__dirname, '../public'),
           keepExtensions: true,
           maxFieldsSize: 2 * 1024 * 1024,
         },
@@ -65,12 +69,12 @@ const controller: Controller[] = [
   },
   // 静态资源
   {
-    path: "/static/:id",
+    path: '/static/:id',
     method: HttpMethodEnum.GET,
     handler: [
       async (ctx, next) => {
-        const srcPath = "/" + ctx.params.id;
-        const opts = { root: path.join(__dirname, "../public") };
+        const srcPath = '/' + ctx.params.id;
+        const opts = { root: path.join(__dirname, '../public') };
         await send(ctx, srcPath, opts);
         next();
       },
@@ -79,17 +83,17 @@ const controller: Controller[] = [
   // 分片上传
   // 检查文件
   {
-    path: "/file/shard/check",
+    path: '/file/shard/check',
     method: HttpMethodEnum.POST,
     handler: [
       async (ctx) => {
-        const { fileKey, shardTotalCount, fileSuffix } = ctx.request.body;
+        const { fileKey, fileSuffix } = ctx.request.body;
         const uploadPath = path.join(__dirname, `../upload`);
         // upload文件夹不存在时要新建一个
         try {
           await fs.access(uploadPath);
         } catch (err) {
-          if (err.code === "ENOENT") {
+          if (err.code === 'ENOENT') {
             await fs.mkdir(uploadPath);
           } else {
             console.log(err);
@@ -106,7 +110,7 @@ const controller: Controller[] = [
           let files = [];
           try {
             files = await fs.readdir(filePath);
-          } catch (e) {
+          } catch {
             fs.mkdir(filePath);
           }
 
@@ -120,27 +124,27 @@ const controller: Controller[] = [
   },
   // 上传分片
   {
-    path: "/file/shard/upload",
+    path: '/file/shard/upload',
     method: HttpMethodEnum.POST,
     handler: [
       koaBody({
         multipart: true,
         formidable: {
-          uploadDir: path.join(__dirname, "../upload"),
+          uploadDir: path.join(__dirname, '../upload'),
           keepExtensions: true,
           maxFieldsSize: 2 * 1024 * 1024,
-          onFileBegin(name, file) {
+          onFileBegin() {
             // console.log(file);
           },
         },
       }),
       async (ctx) => {
         const { fileKey, shardIndex } = ctx.request.body;
-        const file = ctx.request.files.file as any;
-        fs.rename(
-          file.filepath,
-          path.join(__dirname, `../upload/${fileKey}/${shardIndex}`)
-        );
+        const file = ctx.request.files.file;
+        if (Array.isArray(file)) {
+          return;
+        }
+        fs.rename(file.filepath, path.join(__dirname, `../upload/${fileKey}/${shardIndex}`));
 
         ctx.body = AjaxResult.success(`分片${shardIndex}上传成功！`);
       },
@@ -148,29 +152,25 @@ const controller: Controller[] = [
   },
   // 合并分片
   {
-    path: "/file/shard/merge",
+    path: '/file/shard/merge',
     method: HttpMethodEnum.POST,
     handler: [
       async (ctx) => {
         const { fileKey, shardTotalCount, fileSuffix } = ctx.request.body;
-        const shardList = await fs.readdir(
-          path.join(__dirname, `../upload/${fileKey}`)
-        );
+        const shardList = await fs.readdir(path.join(__dirname, `../upload/${fileKey}`));
         if (shardList.length !== shardTotalCount || shardList.length === 0) {
-          ctx.body = AjaxResult.success("切片数量不符！");
+          ctx.body = AjaxResult.success('切片数量不符！');
         } else {
-          const filePath = path.join(
-            __dirname,
-            `../upload/${fileKey}.${fileSuffix}`
-          );
-          await fs.writeFile(filePath, "");
+          const filePath = path.join(__dirname, `../upload/${fileKey}.${fileSuffix}`);
+          await fs.writeFile(filePath, '');
           for (let i = 0; i < shardList.length; i++) {
             const shardPath = path.join(__dirname, `../upload/${fileKey}/${i}`);
-            await fs.appendFile(filePath, await fs.readFile(shardPath));
+            const unit8Array = new Uint8Array(await fs.readFile(shardPath));
+            await fs.appendFile(filePath, unit8Array);
             await fs.unlink(shardPath);
           }
           await fs.rmdir(path.join(__dirname, `../upload/${fileKey}`));
-          ctx.body = AjaxResult.success("合并成功！");
+          ctx.body = AjaxResult.success('合并成功！');
         }
       },
     ],
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import ElementPlus from 'unplugin-element-plus/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ElementPlus({}), visualizer({ open: true })],
+  plugins: [vue(), vueDevTools(), ElementPlus({}), visualizer({ open: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
